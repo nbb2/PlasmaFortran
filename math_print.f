1d393d0a9da637d4a3fab12a43b6b9b822fdf021
bartlen <nbb2@illinois.edu>
2018-12-22 11:28:54 -0600

    A program to read a number and print out a result

---
+++ math_print.f
@@ -0,0 +1,18 @@
+c
+c     Code written by Nathan bartlett //  IDM ID ; bartlen
+c
+c     This program reads a real number and operates on it with several
+c     different types of mathematical operations, it then prints them
+c
+c     -----------------------------------------------------------------
+c
+c     A = Real number input
+c     B = operated number
+c
+c
+c
+      READ *, A
+      B = 2 * A
+      PRINT *, B
+      STOP
+      END
